2ac8c3911195891fbe011fcc3ddf84e9904c324f
Dasol Park <dasol.park615@gmail.com>
2020-02-21 17:55:39 +0900

    Updates

---
+++ precache-manifest.ac46316f1895fbd2eab8ec34c5b92873.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "864addcdae521560fc0f72ea594af0de",
+    "url": "/Dasol_React_Video_Search_With_YouTube_API/index.html"
+  },
+  {
+    "revision": "4fb614eddc3104743eae",
+    "url": "/Dasol_React_Video_Search_With_YouTube_API/static/css/main.165c5e63.chunk.css"
+  },
+  {
+    "revision": "b96f54f0ea879dee3e27",
+    "url": "/Dasol_React_Video_Search_With_YouTube_API/static/js/2.9e89e4ba.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/Dasol_React_Video_Search_With_YouTube_API/static/js/2.9e89e4ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb614eddc3104743eae",
+    "url": "/Dasol_React_Video_Search_With_YouTube_API/static/js/main.30e960e5.chunk.js"
+  },
+  {
+    "revision": "68ad05b93ffcc2000606",
+    "url": "/Dasol_React_Video_Search_With_YouTube_API/static/js/runtime-main.8ea92285.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Dasol_React_Video_Search_With_YouTube_API/precache-manifest.ac46316f1895fbd2eab8ec34c5b92873.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dasol_React_Video_Search_With_YouTube_API/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
