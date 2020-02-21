270f5cd9332b39f686ab982ffa2cf56138c98a1b
Dasol Park <dasol.park615@gmail.com>
2020-02-21 15:22:34 +0900

    Search Bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  onTermSubmit(term) {
+    console.log(term);
+  }
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onTermSubmit={this.onTermSubmit}></SearchBar>
+      </div>
+    );
   }
 }
 
---
+++ src/components/SearchBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+
+    this.props.onTermSubmit(this.state.term);
+  };
+
+  onInputChange = e => {
+    this.setState({ term: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="ui fluid category search">
+        <form
+          className="ui icon input form"
+          onSubmit={this.onFormSubmit}
+          style={{ width: '100%' }}
+        >
+          <input
+            className="prompt"
+            type="text"
+            placeholder="Search Videos..."
+            onChange={this.onInputChange}
+            value={this.state.term}
+          />
+          <i className="search icon"></i>
+        </form>
+        <div className="results"></div>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
