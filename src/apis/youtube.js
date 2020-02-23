9078e01c9c026deaaf078e5dc398b4bb01b3c513
Dasol Park <dasol.park615@gmail.com>
2020-02-23 22:38:53 +0900

    Update maxResults

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
-    maxResults: 20,
+    maxResults: 30,
     type: 'video',
     key: process.env.REACT_APP_YOUTUBE_API_KEY
   }
